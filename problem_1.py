f88260d24d758becec9abb3221115063b612fc1d
Nayra Dourado <91966681+naydourado@users.noreply.github.com>
2024-06-06 21:19:08 -0300

    teste3

--- problem_1.py
+++ problem_1.py
@@ -2,9 +2,9 @@
 def main(cargo, salario):
 
     if cargo == 'junior':
-        return salario*0.15 + salario
+        return salario*.15 + salario
     elif cargo == 'pleno':
-        return salario*0.26 + salario:
+        return salario*0.26 + salario
     elif cargo == 'senior':
         return salario*0.34 + salario
     else:
